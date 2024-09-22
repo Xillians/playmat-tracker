44d87c4807cb86319461995cb09166a20472bc98
Xillians <sandra.toftemo@bjerk.io>
2024-09-22 20:17:25 +0200

    revert to static

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,15 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({
-      paths: {
-        base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-      }
-    })
-  },
-  preprocess: vitePreprocess(),
-
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
