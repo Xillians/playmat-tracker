8ede9211a1a3eebc1581882a9cdc959826e28f40
Xillians <sandra.toftemo@bjerk.io>
2024-09-22 20:03:26 +0200

    try this

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,14 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: '/playmat-tracker',
-		}
-	}
+    kit: {
+        adapter: adapter({
+            fallback: '404.html'
+        }),
+        paths: {
+            base: '/playmat-tracker',
+        }
+    }
 };
 
 export default config;
